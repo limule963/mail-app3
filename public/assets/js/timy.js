7d3f5cc5072b003963a95a08bdc9724823a2c48f
limule963 <kofazia@gmail.com>
2023-03-16 18:58:17 +0000

    deuxieme du 16

---
+++ public/assets/js/timy.js
@@ -0,0 +1,31 @@
+var tabs = document.querySelectorAll('#v-pills-tabContent textarea');
+
+
+for(let element of tabs)
+{
+    
+    
+}
+
+tinymce.init({
+    selector: '#v-pills-tabContent textarea',
+    skin: 'bootstrap',
+    plugins: 'lists, link, image, media, textcolor',
+    toolbar: 'h1 h2 bold italic strikethrough blockquote bullist numlist backcolor forecolor  | link image media |  removeformat help',
+    menubar: 'insert, format,  ',
+    branding: false,
+    height: 300,
+    setup: function(ed) {
+        ed.on('change', function(e) { ed.save(); });
+    }
+    
+  });
+
+//   tinymce.init({ 
+//     selector: 'textarea',
+//     setup: function (editor) {
+//         editor.on('change', function (e) {
+//             editor.save();
+//         });
+//     }
+// });
